c65f9728094ee92aad402b951023e3a89a0904f6
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2020-12-09 17:51:45 -0500

    cleaned up the init command file

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -8,43 +8,47 @@ import { print, emoji, emojis } from "../utils";
 // constants
 import { globalYaml, localYaml, placeholderContent } from "./init.spec";
 
+const rootPath = "./.boilerplate";
+
+const generateFilesAndFolders = () => {
+  // create .boilerplate folder
+  mkdirSync(rootPath);
+  // create global args yml file
+  writeFileSync(`${rootPath}/global.args.yml`, globalYaml);
+  // create component directory (example template out-of-the-box)
+  mkdirSync(`${rootPath}/component`);
+  // create local args yml file
+  writeFileSync(`${rootPath}/component/local.args.yml`, localYaml);
+  // folder with template variable name
+  mkdirSync(`${rootPath}/component/|| name ||`);
+  // file with template variable name and type
+  writeFileSync(
+    `${rootPath}/component/|| name ||/|| name ||.|| type ||`,
+    placeholderContent
+  );
+};
+
 export default class Init extends Command {
   static description = "create a new boilerplate directory";
 
   static flags = {
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    // name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    // force: flags.boolean({ char: "f" }),
   };
 
-  static args = [{ name: "file" }];
-
   async run() {
-    // const { args, flags } = this.parse(Init);
-    const rootPath = "./.boilerplate";
-
     const boilerplateExists = readdirSync("./").some(
       (dir) => dir === ".boilerplate"
     );
 
     if (boilerplateExists) {
       this.error(
-        `${emoji(
-          ":unamused:"
-        )} looks like you already have a .boilerplate folder`
+        `${emoji(":unamused:")} ${print(
+          `looks like you already have a '.boilerplate' folder`,
+          "red"
+        )}`
       );
     } else {
-      mkdirSync(rootPath);
-      writeFileSync(`${rootPath}/global.args.yml`, globalYaml);
-      mkdirSync(`${rootPath}/component`);
-      writeFileSync(`${rootPath}/component/local.args.yml`, localYaml);
-      mkdirSync(`${rootPath}/component/|| name ||`);
-      writeFileSync(
-        `${rootPath}/component/|| name ||/|| name ||.|| type ||`,
-        placeholderContent
-      );
+      generateFilesAndFolders();
       this.log(
         `${emojis([":tropical_drink:", ":dancer:"])} ${print(
           `'.boilerplate' folder has been created in the root of the current directory`

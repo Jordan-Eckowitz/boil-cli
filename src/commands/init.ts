ecf5049208d435366b7dfa29698498d4d66ed7d0
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2020-12-08 22:37:00 -0500

    added chalk, node emojis, added some logic to init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,25 +1,43 @@
-import {Command, flags} from '@oclif/command'
+// packages
+import { mkdirSync, readdirSync } from "fs";
+import { Command, flags } from "@oclif/command";
+
+// utils
+import { print, emoji, emojis } from "../utils";
 
 export default class Init extends Command {
-  static description = 'describe the command here'
+  static description = "create a new boilerplate directory";
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    // name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    // force: flags.boolean({ char: "f" }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(Init)
+    // const { args, flags } = this.parse(Init);
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/jordan/development/boil-cli/src/commands/init.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const boilerplateExists = readdirSync("./").some(
+      (dir) => dir === ".boilerplate"
+    );
+
+    if (boilerplateExists) {
+      this.error(
+        `${emoji(
+          ":unamused:"
+        )} looks like you already have a .boilerplate folder`
+      );
+    } else {
+      mkdirSync("./.boilerplate");
+      this.log(
+        `\n${emojis([":tropical_drink:", ":dancer:"])} ${print(
+          ".boilerplate folder has been created in the root of the current directory"
+        )}\n`
+      );
     }
   }
 }

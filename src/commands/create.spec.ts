5795be822f521c016dd7750577c652ed8b6fcee4
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2020-12-16 19:43:52 -0500

    generate template folder and yml, updated arg name to variable, error catching create command

---
+++ src/commands/create.spec.ts
@@ -0,0 +1,26 @@
+// packages
+import { existsSync, mkdirSync, writeFileSync } from "fs";
+
+const rootPath = "./.boilerplate";
+
+export const templateExists = (name: string) => {
+  return existsSync(`${rootPath}/${name}`);
+};
+
+export const generateTemplate = (name: string, variables: string[]) => {
+  const path = `${rootPath}/${name}`;
+  console.log(variables);
+  mkdirSync(path);
+
+  let ymlContent = variables.reduce((output, variable) => {
+    const str = `# definition of '${variable}' variable
+${variable}: # variable will be called using --${variable}
+    # shorthand: x # variable can be called using -x instead of --${variable}
+    description: ${variable} variable # used in help menu \n\n`;
+    return output + str;
+  }, "");
+
+  if (ymlContent === "") ymlContent = `# add any local variable config here`;
+
+  writeFileSync(`${path}/local.args.yml`, ymlContent);
+};
--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,7 +1,8 @@
 import { Command, flags } from "@oclif/command";
 
 // utils
-import { print, printError } from "../utils";
+import { print, printError, boilerplateExists } from "../utils";
+import { templateExists, generateTemplate } from "./create.spec";
 
 export default class Create extends Command {
   static description = "create a new boilerplate template";
@@ -24,15 +25,37 @@ export default class Create extends Command {
   ];
 
   async run() {
-    const { args, flags } = this.parse(Create);
+    const {
+      args: { name },
+      flags,
+    } = this.parse(Create);
 
-    if (!args.name) {
+    // 1. check that '.boilerplate' directory exists
+    if (!boilerplateExists()) {
+      return this.log(
+        printError(
+          `looks like you don't have a '.boilerplate' directory - run 'boil init' to start a new project`
+        )
+      );
+    }
+
+    // 2. validate that the name argument has been provided
+    if (!name) {
       this.log(printError(`you need to provide a template name`));
       this.log(`\nrun 'boil create ${print("--help")}' for details`);
       return;
     }
 
-    console.log(`args: ${JSON.stringify(args)}`);
+    // 3. validate the template name doesn't exist yet
+    if (templateExists(name)) {
+      return this.log(printError(`template '${name}' already exists`));
+    }
+
+    // 4. generate template
+    const variables = flags.variables ? flags.variables.split(",") : [];
+    generateTemplate(name, variables);
+
+    console.log(`args: ${JSON.stringify(name)}`);
     console.log(`flags: ${JSON.stringify(flags)}`);
   }
 }

865616f1c50342aaeedd1e3d88b9fef1a471e305
Jordan-Eckowitz <jordan.eckowitz@gmail.com>
2021-01-04 21:44:25 -0500

    check for boilerplate dir, check if there are any existing commands

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,7 +3,7 @@ import { Command, flags } from "@oclif/command";
 import { readdirSync, lstatSync } from "fs";
 
 // utils
-import { print, bold } from "../utils";
+import { print, printError, bold, boilerplateExists } from "../utils";
 
 const root = "./.boilerplate";
 
@@ -13,11 +13,30 @@ export default class List extends Command {
   static flags = { help: flags.help({ char: "h" }) };
 
   async run() {
+    // 1. check that '.boilerplate' directory exists
+    if (!boilerplateExists()) {
+      return this.log(
+        printError(
+          `looks like you don't have a '.boilerplate' directory - run 'boil init' to start a new project`
+        )
+      );
+    }
+
     // extract folder names from '.boilerplate' directory
     const commands = readdirSync(root).filter((src) =>
       lstatSync(`${root}/${src}`).isDirectory()
     );
 
+    // 2. if no commands in boilerplate directory then prompt the user to create one
+    if (commands.length === 0) {
+      return this.log(
+        printError(
+          `looks like you haven't created any commands yet - run 'boil create --help' for more information`
+        )
+      );
+    }
+
+    // 3. list all commands
     this.log(`\n${bold("COMMANDS")}`);
     commands.map((cmd) => this.log(`  boil up ${print(cmd, "blue")}`));
     this.log(`\n${bold("OPTIONS")}`);
